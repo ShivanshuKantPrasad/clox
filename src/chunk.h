f39527119e5b89f9ab3e5d49c65cfc537359d4e2
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-06-08 13:40:54 +0530

    Implement Value Types BOOL and NIL

--- src/chunk.h
+++ src/chunk.h
@@ -6,10 +6,17 @@
 typedef enum {
   OP_CONSTANT_LONG,
   OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
   OP_ADD,
   OP_SUBTRACT,
   OP_MULTIPLY,
   OP_DIVIDE,
+  OP_NOT,
   OP_NEGATE,
   OP_RETURN,
 } OpCode;
@@ -33,7 +40,7 @@ void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte, int line);
 size_t addConstant(Chunk *chunk, Value value);
-void writeConstant(Chunk* chunk, Value value, int line);
+void writeConstant(Chunk *chunk, Value value, int line);
 
 void initLine(Line *lines);
 void freeLine(Line *lines);
--- src/debug.c
+++ src/debug.c
@@ -35,6 +35,18 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_RETURN", offset);
   case OP_CONSTANT:
     return constantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_NIL:
+    return simpleInstruction("OP_NIL", offset);
+  case OP_TRUE:
+    return simpleInstruction("OP_TRUE", offset);
+  case OP_FALSE:
+    return simpleInstruction("OP_FALSE", offset);
+  case OP_EQUAL:
+    return simpleInstruction("OP_EQUAL", offset);
+  case OP_GREATER:
+    return simpleInstruction("OP_GREATER", offset);
+  case OP_LESS:
+    return simpleInstruction("OP_LESS", offset);
   case OP_ADD:
     return simpleInstruction("OP_ADD", offset);
   case OP_SUBTRACT:
@@ -45,6 +57,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_MULTIPLY", offset);
   case OP_DIVIDE:
     return simpleInstruction("OP_DIVIDE", offset);
+  case OP_NOT:
+    return simpleInstruction("OP_NOT", offset);
   case OP_CONSTANT_LONG:
     return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset);
   default:
@@ -63,8 +77,8 @@ static int constantInstruction(const char *name, Chunk *chunk, int offset) {
 
 int constantLongInstruction(const char *name, Chunk *chunk, int offset) {
   uint32_t constant = (chunk->code[offset + 1] << 16) +
-          (chunk->code[offset + 2] << 8) +
-          (chunk->code[offset + 3]);
+                      (chunk->code[offset + 2] << 8) +
+                      (chunk->code[offset + 3]);
   printf("%-16s %16d '", name, constant);
   printValue(chunk->constants.values[constant]);
   printf("'\n");
