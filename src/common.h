c7d7d59a4dbd8e57906ad07ef9ff8a8a4bce373f
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-06-10 20:16:32 +0530

    Implement Local Variables

--- src/common.h
+++ src/common.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -6,3 +7,7 @@
 
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
+
+#define UINT8_COUNT (UINT8_MAX + 1)
+
+#endif
--- src/vm.c
+++ src/vm.c
@@ -8,6 +8,7 @@
 #include "value.h"
 
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -136,6 +137,11 @@ static InterpretResult run() {
     case OP_POP:
       pop();
       break;
+    case OP_GET_LOCAL: {
+      uint8_t slot = READ_BYTE();
+      push(vm.stack->values[slot]);
+      break;
+    }
     case OP_GET_GLOBAL: {
       ObjString *name = READ_STRING();
       Value value;
@@ -150,6 +156,11 @@ static InterpretResult run() {
       ObjString *name = READ_STRING();
       tableSet(&vm.globals, name, peek(0));
       pop();
+      break;
+    }
+    case OP_SET_LOCAL: {
+      uint8_t slot = READ_BYTE();
+      vm.stack->values[slot] = peek(0);
       break;
     }
     case OP_SET_GLOBAL: {
